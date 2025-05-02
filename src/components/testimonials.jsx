f16208494d6c23a1f6f37749fdd33c430c0e556f
Manthan-Pelne <manthanpelne1997@gmail.com>
2025-05-02 16:20:46 +0530

    updated Project

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Star } from 'lucide-react';
-import TestimonialCard from './TestimonialCard';
+import TestimonialCard from './testimonialCard';
 
 const Testimonials = () => {
   const testimonials = [
